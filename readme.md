844403114e27d893b6cd34ca22e8fcb8d73050d8
Jayanta <bjayanta.me@gmail.com>
2023-07-20 21:36:54 +0600

    type basics

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Type basics
 
+1. Variable
+2. Function
+3. Array
+4. Object
+
 Thanks.
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,62 @@
-// console.log("Hello Wolrd!");
+// Variable
 
-const country = "Bangladesh"
+// Example 1:
+let playerName = "Mashrafi";
+let age = 35;
 
-console.log(country);
+console.log(playerName);
+
+// playerName = 34; // compile time error
+// age = "Sakib"; // compile time error
+
+console.log(playerName);
+
+
+// Example 2:
+let playerName2;
+
+playerName2 = "Mashrafi";
+playerName2 = 34;
+
+console.log(playerName2);
+
+
+// Function
+
+// Example 3:
+function multiply(a : number, b : number) {
+    return a * b;
+}
+
+console.log(multiply(10, 5));
+// console.log(multiply("Jayanta", 5)); // Error
+
+
+// Array
+
+// Example 4:
+let fruits = ['apple', 'banana', 'orange'];
+let fruits2 = [];
+let fruits3 = ['apple', 3, true];
+
+// fruits.push(34); // error
+
+fruits2.push(34);
+fruits2.push("Mango");
+
+// Error
+// fruits3.push({
+//     name: "Sakib"
+// })
+
+
+// Object
+
+// Example 5:
+let person = {
+    name: "Mashrafi",
+    age: 35,
+    isCaptain: true
+};
+
+// person.country = "Bangladesh"; // Error
