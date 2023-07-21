a71b71e5cdf88492bc6930d4b83416a649f5f6c0
Jayanta <bjayanta.me@gmail.com>
2023-07-21 16:39:04 +0600

    all about Function Signatures

--- output/script.js
+++ output/script.js
@@ -1,4 +1,25 @@
 "use strict";
-// console.log("Hello Wolrd!");
-const country = "Bangladesh";
-console.log(country);
+// Example 1:
+// let myTestFunc: (x: string, y: string) => void
+// Example 2:
+let add;
+add = (a, b) => {
+    console.log(a + b);
+    return a + b;
+};
+// Example 3:
+let calculation;
+calculation = (a, b, c) => {
+    if (c === 'add') {
+        return a + b;
+    }
+    else {
+        return a - b;
+    }
+};
+console.log(calculation(5, 6, 'minus'));
+// Example 4:
+let userObject;
+userObject = (id, data) => {
+    console.log(1);
+};
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,37 @@
-// console.log("Hello Wolrd!");
+// Example 1:
+// let myTestFunc: (x: string, y: string) => void
 
-const country = "Bangladesh"
+// Example 2:
+let add: (x: number, y: number) => number
 
-console.log(country);
+add = (a: number, b: number) => {
+    console.log(a + b);
+    return a + b;
+}
+
+// Example 3:
+let calculation: (x: number, y: number, z: string) => number
+
+calculation = (a: number, b:number, c: string) => {
+    if (c === 'add') {
+        return a + b
+    } else {
+        return a - b
+    }
+}
+
+console.log(calculation(5, 6 , 'minus'));
+
+// Example 4:
+let userObject: (id: number | string, info: {
+    name: string,
+    age: number
+}) => void
+
+userObject = (id: number | string, data: {
+    name: string,
+    age: number
+}) => {
+    console.log(1);
+    
+}
