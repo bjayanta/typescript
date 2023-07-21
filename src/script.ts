de167a082f50117506da1174884f895b9f793b8c
Jayanta <bjayanta.me@gmail.com>
2023-07-21 16:27:03 +0600

    all about function

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,36 @@
-// console.log("Hello Wolrd!");
+// Normal function
+// const myFunc = () => {
+//     console.log("Hello");
+// }
 
-const country = "Bangladesh"
+let myFunc : Function;
 
-console.log(country);
+myFunc = () => {
+    console.log("Hello");
+}
+
+// myFunc = 10; // error
+
+// Optional parameter c
+const yourFunc: Function = (a: string, b: string, c?:string) => {
+    console.log(c);
+    console.log(`Hello ${a} ${b}`);
+}
+
+// yourFunc('A', 'B');
+
+// Set default value
+const yourFunc1: Function = (a: string, b: string, c:string="Test") => {
+    console.log(c);
+    console.log(`Hello ${a} ${b}`);
+}
+
+// yourFunc1('A', 'B');
+
+// Return type
+const yourFunc2: Function = (a: string, b: string, c:string="Test") : string => {
+    return a + b;
+}
+
+console.log(yourFunc2('A', 'B'));
+
