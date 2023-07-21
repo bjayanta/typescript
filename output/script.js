13d478aadf1dd6388545d0d5ce5d346d2d4472f4
Jayanta <bjayanta.me@gmail.com>
2023-07-21 16:45:40 +0600

    All about classes

--- output/script.js
+++ output/script.js
@@ -1,4 +1,23 @@
 "use strict";
-// console.log("Hello Wolrd!");
-const country = "Bangladesh";
-console.log(country);
+// Class
+class Player {
+    // Method + Parameters
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    // Method
+    play() {
+        console.log(`${this.name} from ${this.country} is playing!`);
+    }
+}
+// Object
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh');
+const sakib = new Player('Sakib', 38, 'Bangladesh');
+// Custom type
+const players = [];
+// players.push('Sakib') // error
+players.push(mashrafi);
+players.push(sakib);
+console.log(players);
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,33 @@
-// console.log("Hello Wolrd!");
+// Class
+class Player {
+    // Instance variables
+    name: string;
+    age: number;
+    country: string;
 
-const country = "Bangladesh"
+    // Method + Parameters
+    constructor(n: string, a: number, c: string) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    } 
 
-console.log(country);
+    // Method
+    play() {
+        console.log(`${this.name} from ${this.country} is playing!`);
+    }
+
+}
+
+// Object
+const mashrafi = new Player('Mashrafi', 40, 'Bangladesh')
+const sakib = new Player('Sakib', 38, 'Bangladesh')
+
+// Custom type
+const players: Player[] = [];
+
+// players.push('Sakib') // error
+players.push(mashrafi)
+players.push(sakib)
+
+console.log(players);
